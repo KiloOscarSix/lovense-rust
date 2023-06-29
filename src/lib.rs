222e4049954d275db159ade13443b012de74ed57
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-06-29 07:09:56 +0100

    created vibrate function

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,23 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use reqwest::Client;
+use serde_json::json;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub async fn vibrate(strength: i32, time: f32, stop_previous: bool) {
+    let lovense_local_ip = "localhost";
+    let lovense_http_port = 8000;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    let data = json!({
+        "command": "Function",
+        "action": format!("Vibrate:{}", strength),
+        "timeSec": time,
+        "stopPrevious": stop_previous as i32,
+        "apiVer": 1
+    });
+
+    let client = Client::new();
+    let url = format!("http://{}:{}/command", lovense_local_ip, lovense_http_port);
+
+    match client.post(&url).json(&data).send().await {
+        Ok(_) => println!("Vibrate command sent"),
+        Err(e) => println!("Error sending vibrate command: {}", e)
     }
 }
