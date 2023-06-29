78e3431c5a3100d4b554a4377e201e62af5e07e6
Oscar Six <26691547+KiloOscarSix@users.noreply.github.com>
2023-06-29 07:24:13 +0100

    improve argument name

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,14 @@
 use reqwest::Client;
 use serde_json::json;
 
-pub async fn vibrate(strength: i32, time: f32, stop_previous: bool) {
+pub async fn vibrate(strength: i32, time_sec: f32, stop_previous: bool) {
     let lovense_local_ip = "localhost";
     let lovense_http_port = 8000;
 
     let data = json!({
         "command": "Function",
         "action": format!("Vibrate:{}", strength),
-        "timeSec": time,
+        "timeSec": time_sec,
         "stopPrevious": stop_previous as i32,
         "apiVer": 1
     });
